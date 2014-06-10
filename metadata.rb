4e0c880b4dfc1185754995c0ccc7f6d53bcf0d35
Mikhail Vaysman <mikhail.vaysman@gmail.com>
2014-06-10 17:27:32 +0400

    metadata added

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "set-proxy"
+maintainer       "Mikahil Vaysman"
+maintainer_email "mikhail.vaysman@gmail.com"
+license          "BSD License"
+description      "Set proxy environment variable and set proxy for apt-get"
+long_description "Set proxy environment variable and set proxy for apt-get"
+version          "1.0"
+supports         "ubuntu"
